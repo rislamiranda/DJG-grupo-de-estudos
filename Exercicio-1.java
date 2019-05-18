32888f831c4ac4d164510d19a1e82ba148a097a1
Risla <rislamiranda@gmail.com>
2019-05-17 21:02:04 -0300

    Exercicio 1

---
+++ Exercicio-1.java
@@ -0,0 +1,13 @@
+class MeusDados {
+    public static void main(String[] args) {
+        String nome = "Risla";
+        int idade = 29;
+        double altura = 1.57;
+        boolean gostoDeJava = true;
+
+        System.out.println("Meu nome é: " + nome);
+        System.out.println("Minha idade é: " + idade);
+        System.out.println("Minha altura é: " + altura);
+        System.out.println("Eu gosto de Java: " + gostoDeJava);
+    }
+}
